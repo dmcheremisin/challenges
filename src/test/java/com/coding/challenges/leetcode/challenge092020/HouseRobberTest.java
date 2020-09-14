52cc272ba2ec9853adc1073642890702c492d715
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-09-14 19:38:44 +0300

    HouseRobber recursive solution

--- src/test/java/com/coding/challenges/leetcode/challenge092020/HouseRobberTest.java
+++ src/test/java/com/coding/challenges/leetcode/challenge092020/HouseRobberTest.java
@@ -37,4 +37,11 @@ public class HouseRobberTest {
         int robbed = houseRobber.rob(new int[]{4, 1, 2, 7, 5, 3, 1});
         assertEquals(14, robbed);
     }
+
+    @Test
+    public void rob6() {
+        int robbed = houseRobber.rob(new int[]{183, 219, 57, 193, 94, 233, 202, 154, 65, 240, 97, 234, 100, 249, 186,
+                66, 90, 238, 168, 128, 177, 235, 50, 81, 185, 165, 217, 207, 88, 80, 112, 78, 135, 62, 228, 247, 211});
+        assertEquals(3365, robbed);
+    }
 }
